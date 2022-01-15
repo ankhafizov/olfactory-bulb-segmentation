834861370359d59715b449854ac24b4c0e9476bf
Anvar <ankhafizov1998@yandex.ru>
2022-01-15 21:31:20 +0300

    final interface

--- http_client/app/views.py
+++ http_client/app/views.py
@@ -4,17 +4,19 @@ from flask import render_template, request
 import requests as request_http
 import numpy as np
 import cv2
-from PIL import Image
+
+import matplotlib.pyplot as plt
+from skimage import exposure
 
 
 APP_ROOT = "app"
 OUTPUT_SAVE_PATH = f"static/img/output.png"
 
 
-def decode_image_file_to_Image(image_file):
+def decode_image_file_to_numpy(image_file):
     nparr = np.frombuffer(image_file, np.uint8)
     img_np = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
-    return Image.fromarray(img_np)
+    return img_np
 
 
 @app.route('/', methods=["POST", "GET"])
@@ -23,10 +25,21 @@ def upload_file():
         if request.files:
 
             image = request.files["image"]
-            response = request_http.post("http://127.0.0.1:5001/upload-raw-image", files={"image" : image})
+            image_orig = decode_image_file_to_numpy(image.read())
+            image = image.seek(0)
 
-            image_mask = decode_image_file_to_Image(response.content)
-            image_mask.save(os.path.join(APP_ROOT, OUTPUT_SAVE_PATH))
+            response = request_http.post("http://127.0.0.1:5001/upload-raw-image", files={"image" : request.files["image"]})
+
+            image_mask = decode_image_file_to_numpy(response.content)
+            image_orig = exposure.equalize_adapthist(image_orig, clip_limit=0.03)
+            # print(image)
+
+            fig, ax = plt.subplots(1, 1, figsize=(10, 10), constrained_layout=True)
+            image_mask = np.array(image_mask, dtype=bool)
+            ax.imshow(image_orig, cmap="gray")
+            ax.imshow(~image_mask, alpha=0.3, cmap="Reds")
+            ax.axis("off")
+            fig.savefig(os.path.join(APP_ROOT, OUTPUT_SAVE_PATH))
 
             return render_template("main.html", output_img_url=OUTPUT_SAVE_PATH)
     
