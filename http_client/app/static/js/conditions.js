e467011fe3a0e644688617b2367de55c27e84fca
Anvar <ankhafizov1998@yandex.ru>
2022-01-16 19:49:03 +0300

    added checkbox checkers

--- http_client/app/static/js/conditions.js
+++ http_client/app/static/js/conditions.js
@@ -1,1 +1,7 @@
-console.log("works good")
+$( "#submit" ).click(function() {
+    var checked = $("#frmText input:checked").length > 0;
+    if (!checked){
+        alert("Please check at least one option has been selected");
+        return false;
+    }
+});
