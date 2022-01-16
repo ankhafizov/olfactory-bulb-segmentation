9cb9420dd7fa5185b581154aabb3cb5c37b44512
Anvar <ankhafizov1998@yandex.ru>
2022-01-16 20:36:28 +0300

    fix submit selection

--- http_client/app/static/js/conditions.js
+++ http_client/app/static/js/conditions.js
@@ -1,6 +1,18 @@
+var FILE_SELECTED = false;
+
+
 $( "#submit" ).click(function() {
     var checked = $("#inputForm input:checked").length > 0;
-    if (!checked){
+
+    if(!FILE_SELECTED && !checked) { // returns true if the string is not empty
+        alert("Please select image file and check at least one option");
+        return false;
+    }
+    else if(!FILE_SELECTED){
+        alert("Please select image file");
+        return false;
+    }
+    else if(!checked){
         alert("Please check at least one option has been selected");
         return false;
     }
@@ -9,18 +21,7 @@ $( "#submit" ).click(function() {
 
 $(document).on('change', '.custom-file-input', function () {
     let fileName = $(this).val().replace(/\\/g, '/').replace(/.*\//, '');
-    console.log($( "#fileInputLabel" ).value)
     $( "#fileInputLabel" ).text(fileName);
-});
-
-
-$("#submit").click(function() { 
-    // bCheck is a input type button
-    var fileName = $("#file1").val();
-
-    if(fileName) { // returns true if the string is not empty
-        alert(fileName + " was selected");
-    } else { // no file was selected
-        alert("no file selected");
-    }
+    FILE_SELECTED = true
+    console.log(FILE_SELECTED)
 });
