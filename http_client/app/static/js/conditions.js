136446bc0168179efe9b624b41f3ad80a3b1b3af
Anvar <ankhafizov1998@yandex.ru>
2022-01-16 20:29:26 +0300

    add file selection label

--- http_client/app/static/js/conditions.js
+++ http_client/app/static/js/conditions.js
@@ -1,7 +1,26 @@
 $( "#submit" ).click(function() {
-    var checked = $("#frmText input:checked").length > 0;
+    var checked = $("#inputForm input:checked").length > 0;
     if (!checked){
         alert("Please check at least one option has been selected");
         return false;
+    }
+});
+
+
+$(document).on('change', '.custom-file-input', function () {
+    let fileName = $(this).val().replace(/\\/g, '/').replace(/.*\//, '');
+    console.log($( "#fileInputLabel" ).value)
+    $( "#fileInputLabel" ).text(fileName);
+});
+
+
+$("#submit").click(function() { 
+    // bCheck is a input type button
+    var fileName = $("#file1").val();
+
+    if(fileName) { // returns true if the string is not empty
+        alert(fileName + " was selected");
+    } else { // no file was selected
+        alert("no file selected");
     }
 });
