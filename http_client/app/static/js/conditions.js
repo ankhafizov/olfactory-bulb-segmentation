255cd0d9e336d96fbbcd1355b1d0e5bda36759b2
Anvar <ankhafizov1998@yandex.ru>
2022-01-16 21:10:23 +0300

    added progress bar

--- http_client/app/static/js/conditions.js
+++ http_client/app/static/js/conditions.js
@@ -1,27 +1,29 @@
 var FILE_SELECTED = false;
 
 
-$( "#submit" ).click(function() {
+$("#submit").click(function () {
     var checked = $("#inputForm input:checked").length > 0;
 
-    if(!FILE_SELECTED && !checked) {
+    if (!FILE_SELECTED && !checked) {
         alert("Please select image file and check at least one option");
         return false;
     }
-    else if(!FILE_SELECTED){
+    else if (!FILE_SELECTED) {
         alert("Please select image file");
         return false;
     }
-    else if(!checked){
+    else if (!checked) {
         alert("Please check at least one option has been selected");
         return false;
     }
+
+    $("#progressLoading").removeClass('d-none');
 });
 
 
 $(document).on('change', '.custom-file-input', function () {
     let fileName = $(this).val().replace(/\\/g, '/').replace(/.*\//, '');
-    $( "#fileInputLabel" ).text(fileName);
+    $("#fileInputLabel").text(fileName);
     FILE_SELECTED = true
     console.log(FILE_SELECTED)
 });
